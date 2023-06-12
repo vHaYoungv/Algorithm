557ff26ed1ced72f4ed49875840afb2383b11604
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-06-12 13:43:11 +0900

    # answer[-1] 은 빈 배열일 때 오류가 나지만, answer[-1:]은 오류가 나지 않는다.

---
+++ 프로그래머스/고득점 Kit/[스택큐] 기능개발.py
@@ -0,0 +1,29 @@
+# 내 풀이
+def solution(progresses, speeds):
+    time = []
+    for i, x in enumerate(progresses):
+        time.append((100 - x) // speeds[i] + (((100 - x) % speeds[i]) and 1))
+
+    i = 0
+    temp = []
+    ret = []
+    for x in time:
+        if temp == []:
+            temp.append(x)
+        if temp[-1] < x:
+            ret.append(i)
+            i = 0
+            temp.append(x)
+        i += 1
+    return ret + [i]
+
+# 참고 풀이
+# zip을 이용해서, p와 s를 간단하게 표현
+def solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]

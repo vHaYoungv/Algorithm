5a1a5a8b488b555cca9bbd02ce0bd9d79c1d13a2
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-06-15 17:50:52 +0900

    # 큐로 한쪽에서만 꺼낼 것이라면 굳이 deque()로 변환하지않고 정렬을 reverse=True로 한 후, pop() 을 이용하자 # 힙에는 '조건 맞는 것들 중' 정렬이 필요한 값들을 넣는다.

--- 프로그래머스/고득점 Kit/[힙] 더 맵게.py
+++ 프로그래머스/고득점 Kit/[힙] 더 맵게.py
@@ -3,22 +3,12 @@
 # 런타임 에러: 아예 불가능한 경우 return -1 를 고려하지 않음
 
 import heapq as hq
-
-
 def solution(scoville, K):
-    answer = 0
-
     scoville.sort()
-
+    cnt = 0
     while len(scoville) >= 2 and scoville[0] < K:
-        a = hq.heappop(scoville)
-        b = hq.heappop(scoville)
+        a, b = hq.heappop(scoville), hq.heappop(scoville)
         hq.heappush(scoville, a + 2 * b)
-        answer += 1
+        cnt += 1
+    return cnt if scoville[0] >= K else -1
 
-    if scoville[0] < K:
-        return -1
-
-    return answer
-
-[참고 풀이]

08b6bb9304ffb528ff5a485e78553016dbfcb1a9
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-06-22 17:19:58 +0900

    # product의 repeat변수를 이용하여 더 쉽게 풀이할 수 있다.

--- 프로그래머스/고득점 Kit/[완전탐색] 모음사전.py
+++ 프로그래머스/고득점 Kit/[완전탐색] 모음사전.py
@@ -15,7 +15,20 @@ def solution(word):
     dic = sorted(list(set(dic)))
     return dic.index(word)
 
+[내 풀이2]
+# 참고 풀이의 product 개념을 적용하여 직접 다시 풀어본 풀이
+from itertools import product
+def solution(word):
+    dic = []
+    lst = ['A', 'E', 'I', 'O', 'U']
+    for i in range(1, 6):
+        for x in product(lst, repeat=i):
+            dic.append(''.join(x))
+    dic.sort()
+    return dic.index(word)+1
+
 [참고 풀이]
+# product의 repeat변수를 이용하여 더 쉽게 풀이할 수 있다.
 from itertools import product
 
 solution = lambda word: sorted(["".join(c) for i in range(5) for c in product("AEIOU", repeat=i+1)]).index(word) + 1

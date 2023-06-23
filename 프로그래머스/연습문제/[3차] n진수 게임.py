bac6d6a85b51d9bc4bedb424defc69bca8530a46
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-06-23 19:33:37 +0900

    # 문자열로 처리할 수 있었는데 리스트로 풀이하려해서 코드가 복잡해졌다.

--- 프로그래머스/연습문제/[3차] n진수 게임.py
+++ 프로그래머스/연습문제/[3차] n진수 게임.py
@@ -9,8 +9,6 @@ def tenToN(x, n):
     return str[::-1]
 
 def solution(n, t, m, p):
-    answer = ''
-
     num_list = []
     num = 0
     while len(num_list)<t*m:
@@ -20,12 +18,30 @@ def solution(n, t, m, p):
 
     return ''.join(num_list[p-1::m][:t])
 
+[내 풀이]
+def tenToN(x, n):
+    nums = {k:v for k, v in zip(range(0,17), ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'])}
+    str = ''
+    while x>=n:
+        str += nums[x%n]
+        x = x//n
+    str += nums[x%n]
+    return str[::-1]
+
+def solution(n, t, m, p):
+    numbers = '' #굳이 리스트로 값을 받을 필요가 없었다. 리턴 값도 문자열이고, 슬라이싱이 되기 때문이다.
+    num = 0
+    while len(numbers)<t*m:
+        numbers+=tenToN(num, n)
+        num += 1
+    return numbers[p-1:t*m:m] #t*m까지 구하면 된다
+
 [참고 풀이]
 def solution(n, t, m, p):
     data = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
-    numbers = "0"
+    numbers = "0" #나랑 다르게 문자열에 데이터를 쌓아 반환. 이게 더 효율적이었다.
     for number in range(1, t*m):
-        temp = []
+        temp = [] #생각보다 진법 변환이 쉬워서 따로 함수 정의하지 않아도 되었을 수도 있겠다.
         while number > 0:
             temp.append(data[number%n])
             number //= n

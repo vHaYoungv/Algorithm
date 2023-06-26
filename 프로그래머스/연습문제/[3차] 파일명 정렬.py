40ae1911803bffb3c3fd7ec14aeaff060f03f21e
하영 <134265313+vHaYoungv@users.noreply.github.com>
2023-06-26 22:15:34 +0900

    # 1,2,3 순위의 정렬 기준이 있다고 했을 때, 3순위의 정렬 기준은 '원래 리스트 순서'라면 # '2순위 정렬, 1순위 정렬 순으로 정렬을 하면 1,2,3순위로 정렬이 된다' 는 접근 방법이 새로웠다.

--- 프로그래머스/연습문제/[3차] 파일명 정렬.py
+++ 프로그래머스/연습문제/[3차] 파일명 정렬.py
@@ -9,7 +9,7 @@ def solution(files):
     answer.sort(key=lambda file:(p1.search(file).group().lower(), int(p2.search(file).group()), files.index(file)))
 
     return answer
-# 참고풀이
+# 참고 풀이
 # 1,2,3 순위의 정렬 기준이 있다고 했을 때,
 # 3순위의 정렬 기준은 '원래 리스트 순서'라면
 # 2순위 정렬, 1순위 정렬 순으로 정렬을 하면 1,2,3순위로 정렬이 된다. 는 접근 방법이 새로웠다.
